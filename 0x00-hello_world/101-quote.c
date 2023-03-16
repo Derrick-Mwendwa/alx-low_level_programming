08cbab18f8bb78a8ad83080787d69b8d14b44617
Derrick Mwendwa <derekmwendwa14@gmail.com>
2023-03-16 12:24:10 -0700

    Added 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
