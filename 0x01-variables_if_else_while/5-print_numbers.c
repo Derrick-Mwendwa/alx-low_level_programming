f3d85e870944fd5be373d94d2a631ecf85f31482
Derrick Mwendwa <derekmwendwa14@gmail.com>
2023-03-17 12:25:21 -0700

    Added 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -main function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		printf("%i", x);
+	}
+	printf("\n");
+
+	return (0);
+}
