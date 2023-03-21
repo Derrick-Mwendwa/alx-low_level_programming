b4fdfd5b10918b93deaf6d755194b9a5766f02e6
Derrick Mwendwa <derekmwendwa14@gmail.com>
2023-03-21 16:39:09 -0700

    Added 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -15,7 +15,10 @@ int print_last_digit(int n)
 
 	last_digit = n % 10;
 
-	_putchar(last_digit + 48);
+	if (last_digit < 0)
+		last_digit = -last_digit;
+
+	_putchar(last_digit + '0');
 
 	return (last_digit);
 }
