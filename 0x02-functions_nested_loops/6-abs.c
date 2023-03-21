13e62c841ded4f4028397803e67285c48cf4fbe5
Derrick Mwendwa <derekmwendwa14@gmail.com>
2023-03-21 16:25:35 -0700

    Added 6-abs.c) '

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - gets the absolute value of n
+ * @n: input number
+ *
+ * Return: absolute value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
