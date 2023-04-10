fea8ce748e04e07a805907908bbb98a1edc9bd73
Derrick Mwendwa <derekmwendwa14@gmail.com>
2023-04-10 12:44:56 -0700

    task two

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,9 +1,14 @@
+#include "main.h"
+
 /**
- * _atoi - empty
+ * _atoi - empty function as placeholder
  * @s: input
  *
  * Return: int
  */
 int _atoi(char *s)
 {
+	_putchar(s[0]);
+
+	return (0);
 }
