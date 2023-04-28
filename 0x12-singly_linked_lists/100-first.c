36000ca7578c88d20e53cf91d1717086dca43db7
Derrick Mwendwa <derekmwendwa14@gmail.com>
2023-04-28 14:47:56 -0700

    extra

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void __attribute__((constructor)) before_main(void);
+
+/**
+ * before_main - prints a string
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
