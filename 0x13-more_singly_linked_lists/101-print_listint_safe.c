a8716552dabaa98aa71bebdb7dbf9e629b215a11
Derrick Mwendwa <derekmwendwa14@gmail.com>
2023-05-03 10:47:56 -0700

    101 update

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,86 +1,30 @@
-#include "lists.h"
 #include <stdio.h>
-
-size_t looped_listint_len(const listint_t *head);
-size_t print_listint_safe(const listint_t *head);
+#include "lists.h"
 
 /**
- * looped_listint_length - counts number of unique elements
- * @head: pointer to the head address
+ * print_listint_safe - safely output a linked list
+ * @head: pointer to head address
  *
- * Return: number of unique elements otherwise 0
- */
-size_t looped_listint_length(const listint_t *head)
-{
-	const listint_t *x, *y;
-	size_t nodes = 1;
-
-	if (head == NULL || head->next == NULL)
-		return (0);
-
-	x = head->next;
-	y = (head->next)->next;
-
-	while (y)
-	{
-		if (x == y)
-		{
-			x = y;
-			while (x != y)
-			{
-				nodes++;
-				x = x->next;
-				y = y->next;
-			}
-
-			x = x->next;
-			while (x != y)
-			{
-				nodes++;
-				x = x->next;
-			}
-
-			return (nodes);
-		}
-
-		x = x->next;
-		y = (y->next)->next;
-	}
-
-	return (0);
-}
-
-/**
- * print_listint_safe - outputs listint_t list
- * @head: pointer to the head address
- *
- * Return: number of nodes
+ * Return: nodes in list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t n, index = 0;
+	size_t x = 0;
+	long int diff;
 
-	n = looped_listint_length(head);
-
-	if (n == 0)
+	while (head)
 	{
-		for (; head != NULL; n++)
-		{
-			printf("[%p] %d\n", (void *)head, head->n);
+		diff = head - head->next;
+		x++;
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (diff > 0)
 			head = head->next;
+		else
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;
 		}
 	}
 
-	else
-	{
-		for (index = 0; index < n; index++)
-		{
-			printf("[%p] %d\n", (void *)head, head->n);
-			head = head->next;
-		}
-
-		printf("-> [%p] %d\n", (void *)head, head->n);
-	}
-
-	return (n);
+	return (x);
 }
